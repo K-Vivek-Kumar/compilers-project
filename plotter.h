7dfc68206ea90b8d166a75e3104116e404f25148
PATHLAVATH <102509181+PATHLAVATH@users.noreply.github.com>
2023-11-23 22:29:28 +0530

    Update plotter.h

--- plotter.h
+++ plotter.h
@@ -1,1 +1,9 @@
+#ifndef PLOTTER_H
+#define PLOTTER_H
 
+#include <vector>
+#include <utility>
+
+void generatePythonScript(const std::vector<std::pair<double, double>> &points, const std::vector<std::pair<double, double>> &lines, const std::vector<std::pair<double, double>> &polygons);
+
+#endif
